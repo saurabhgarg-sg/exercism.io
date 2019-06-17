09a9b556c4494fc64a5d6dad6cd4a0531af93e29
Saurabh Garg <20387189+saurabhgarg-sg@users.noreply.github.com>
2019-06-17 13:41:14 -0700

    Address review comments

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,14 +1,12 @@
-// twofer implements second excercise based on theatrical cabling device
-
+// Package twofer implements second exercise based on theatrical cabling device
 package twofer
 
 import "fmt"
 
-// ShareWith implements the name replacement for this excercise
+// ShareWith implements the name replacement for this exercise
 func ShareWith(name string) string {
-	if len(name) == 0{
+	if len(name) == 0 {
 		name = "you"
 	}
 	return fmt.Sprintf("One for %s, one for me.", name)
 }
-	 
